93245bbe4e802776e4fc87e8440d10c484147107
f3mshep <superbiscuit@gmail.com>
2018-07-27 14:20:02 -0700

    added all solutions to unionfind practice work

--- practice/union_find/solutions.rb
+++ practice/union_find/solutions.rb
@@ -1,4 +1,63 @@
 # Question 1
+# making the assumption that the log file is an integer value
+
+class UF
+  # these technically should be private
+  # interesting to call nodes after calling other
+  # methods to see how root changes data structure
+  attr_accessor :nodes, :sizes, :largest
+
+  def initialize(size)
+    self.nodes = []
+    self.sizes = []
+
+    size.times do |node| 
+      nodes << node
+      sizes << 1
+    end
+  end
+
+  def connected(p, q)
+    # check if nodes connected by seeing if they share
+    # a common root
+    root(p) == root(q)
+  end
+
+  def union(p, q, time)
+    # join 2 distinct nodes
+    i = root(p)
+    j = root(q)
+    return if (i == j)
+    if (sizes[i] < sizes[j])
+      # if tree i is smaller than tree j,
+      # i now points to j's root
+      nodes[i] = j
+      sizes[j] += sizes[i]
+    else
+      nodes[j] = i
+      sizes[i] += sizes[j]
+    end
+    return time if all_connected(j) || all_connected(i)
+  end
+
+  def all_connected(node)
+    return sizes[i] == nodes.size
+  end
+
+  private
+
+  def root(node)
+    # get the root of the node
+    while node != nodes[node]
+      # make each node point to its grandparent
+      # speeds up computation 
+      nodes[node] = nodes[nodes[node]]
+      node = nodes[node]
+    end
+    node
+  end
+
+end
 
 # Question 2
 
