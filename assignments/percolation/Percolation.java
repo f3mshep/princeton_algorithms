e33e345ad2a594d9900b06a16802afb9b5694480
f3mshep <superbiscuit@gmail.com>
2018-08-01 09:19:09 -0700

    finished

--- assignments/percolation/Percolation.java
+++ assignments/percolation/Percolation.java
@@ -1,26 +1,22 @@
-import edu.princeton.cs.algs4.UF;
-
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import java.util.ArrayList;
+
+
 
 public class Percolation {
 
     private int[][] grid;
     private int openSites;
-    private UF uf;
+    private WeightedQuickUnionUF uf;
     private int top;
     private int bot;
     private boolean percolated;
 
     public  Percolation(int n) {
         // create n-by-n grid, with all sites blocked
-
-        // create an array of arrays to reprsent a grid.
-        // elements will be integers 0,1 or 2
-        // 0 indicates closed, 1 indicates open, 2 indicates full
-
         grid = new int[n][n];
         int len = (n * n) + 2;
-        uf = new UF(len);
+        uf = new WeightedQuickUnionUF(len);
         top = len - 1;
         bot = len - 2;
         // connect top and bottom rows to virtual top and bot sites
@@ -28,17 +24,11 @@ public class Percolation {
 
     public void open(int row, int col) {
         // open site (row, col) if it is not open already
-        // REMBMER: row and col ARE NOT INDEX VALUES! add 1 to each to access proper arr coord
-        // CHANGE OPEN TO ALSO UNION IF SITE IS IN TOP OR BOT ROW
-
         if (!isOpen(row, col)) {
             // System.out.println("opening row:" + row + " col: " + col);
             int current = coordToValue(row, col);
             if(row == 1) union(current, top);
-            if(row == grid.length && !percolated) union(current, bot);
-
             ArrayList<Integer> open = getAdjacentOpen(row, col);
-
             int i = row - 1;
             int j = col - 1;
             openSites += 1;
@@ -46,15 +36,15 @@ public class Percolation {
             for (int site : open) {
                 union(site, current);
             }
-
+            if (row == grid.length && !percolated && connected(top, current)) {
+                union(current, bot);
+            }
         }
 
     }
 
     public boolean isOpen(int row, int col) {
         // is site (row, col) open?
-        // REMBMER: row and col ARE NOT INDEX VALUES! add 1 to each to access proper arr coord
-        // return true if site is over 0
         int i = row - 1;
         int j = col - 1;
         if(!inBounds(i,j)) throw new IllegalArgumentException();
@@ -63,9 +53,6 @@ public class Percolation {
 
     public boolean isFull(int row, int col) {
         // is site (row, col) full?
-
-        // REMBMER: row and col ARE NOT INDEX VALUES! add 1 to each to access proper arr coord
-        // returns true if UF site is connected to top
         int site = coordToValue(row, col);
         if(!inBounds(row - 1,col - 1)) throw new IllegalArgumentException();
         return connected(top, site);
@@ -73,23 +60,11 @@ public class Percolation {
 
     public int numberOfOpenSites() {
         // number of open sites
-
-        // update this whenever a site is opened
-        // return whatever that variable is
         return openSites;
     }
 
     public boolean percolates() {
         // does the system percolate?
-
-        // this will incorporate the UF class
-        // Two possible approaches:
-        // represent the whole grid as one UF class
-        // or
-        // represent each row as one UF class
-        // either way, top and bottom of each UF class will point to
-        // "virtual" top and bottom sites, which point to every site on top and
-        // bottom
 
         percolated = connected(top, bot);
         return percolated;
@@ -134,8 +109,6 @@ public class Percolation {
         if (j < 0 || j >= grid.length ) return false;
         return true;
     }
-
-
 
     private void union(int p, int q) {
         uf.union(p, q);
--- assignments/percolation/PercolationStats.java
+++ assignments/percolation/PercolationStats.java
@@ -2,12 +2,16 @@ import java.util.Arrays;
 
 public class PercolationStats {
 
-    double [] results;
+    private static final double PERCENTILE = 1.96;
+    private double [] results;
+    private int t;
 
     public PercolationStats(int n, int trials) {
         // perform trials independent experiments on an n-by-n grid
+        if (n < 1 || trials < 1) throw new IllegalArgumentException();
         results = new double[trials];
         int count = 0;
+        t = trials;
         while (count < trials) {
             results[count] = performTrial(n);
             count++;
@@ -24,16 +28,24 @@ public class PercolationStats {
     }
     public double confidenceLo() {
         // low  endpoint of 95% confidence interval
-        return 0.0;
+        return mean() - (PERCENTILE * stddev()) / Math.sqrt(t);
     }
     public double confidenceHi() {
         // high endpoint of 95% confidence interval
-        return 0.0;
+        return mean() - (PERCENTILE * stddev()) / Math.sqrt(t);
+    }
+
+    private String printConf() {
+        return "[" + confidenceLo() +","+ confidenceHi() + "]";
     }
 
     public static void main(String[] args) {
-        PercolationStats test = new PercolationStats(72, 100);
-        System.out.println("Mean: " + test.mean());
+        PercolationStats client = new PercolationStats(Integer.parseInt(args[0]),
+                                                       Integer.parseInt(args[1]));
+        System.out.println("mean                    = " + client.mean());
+        System.out.println("stddev                  = " + client.stddev());
+        System.out.println("95% confidence interval = " + client.printConf());
+
     }
 
     private double performTrial(int n) {
